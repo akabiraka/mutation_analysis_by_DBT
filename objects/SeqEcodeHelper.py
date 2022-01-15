6504165ba0125f0662eeff19f1c1f229bcb0cc23
Anowarul Kabir <akabir0101@gmail.com>
2022-01-15 10:08:38 -0500

    cutting sub-seq and blosum50 added

---
+++ objects/SeqEcodeHelper.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append("../mutation_analysis_by_DBT")
+
+import numpy as np
+import re
+
+class SeqEcodeHelper(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.AA={"A":0,"R":1,"N":2,"D":3,"C":4,"Q":5,"E":6,"G":7,"H":8,"I":9,"L":10,"K":11,"M":12,"F":13,"P":14,"S":15,"T":16,"W":17,"Y":18,"V":19}
+
+    def read_blosum(self, path="data/blosum50.txt"):
+        blosum = []
+        with open(path,"r") as f:
+            for line in f:
+                blosum.append([(float(i))/10 for i in re.split("\t",line)])
+                #The values are rescaled by a factor of 1/10 to facilitate training
+        return blosum
+
+
+    def cut_mutation_region(self, seq, mutation_pos, sub_seq_len=15):
+        j=1
+        sub_seq_len-=1
+        from_index, to_index= 0, len(seq)-1
+        while(sub_seq_len>0):
+            # pointer going right direction
+            if mutation_pos+j in range(len(seq)): 
+                to_index=mutation_pos+j
+                sub_seq_len-=1
+            # pointer going left direction
+            if mutation_pos-j in range(len(seq)):
+                from_index=mutation_pos-j
+                sub_seq_len-=1
+
+            j+=1
+            # print(len(seq[from_index:to_index+1]), seq[from_index:to_index+1], from_index, to_index, sub_seq_len)
+        return seq[from_index:to_index+1]
+
+    
+
+# sample usage
+# seq_encode_helper = SeqEcodeHelper()    
+
+# blosum_matrix = seq_encode_helper.read_blosum(path="data/blosum50.txt")
+# # print(len(blosum_matrix), len(blosum_matrix[0])) # row, col
+
+# seq = "MKLF"
+# mutation_pos = 3
+# seq_encode_helper.cut_mutation_region(seq, mutation_pos, 3)
